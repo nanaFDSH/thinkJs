83420190659ea7a4bc7a8fe15b5de828186a0ac5
wnn <wnn@wnndeMacBook-Pro.local>
2017-12-20 23:25:49 +0800

    change config

--- src/config/adapter.js
+++ src/config/adapter.js
@@ -11,16 +11,16 @@ const isDev = think.env === 'development';
  * @type {Object}
  */
 exports.cache = {
-  type: 'redis',
-  common: {
-    timeout: 24 * 3600 * 1000 // millisecond
-  },
-  redis: {
-    handle: redisCache,
-    port: 6379,
-    host: '127.0.0.1',
-    password: '123456'
-  }
+    type: 'redis',
+    common: {
+        timeout: 24 * 3600 * 1000 // millisecond
+    },
+    redis: {
+        handle: redisCache,
+        port: 6379,
+        host: '127.0.0.1',
+        password: '123456'
+    }
 }
 
 /**
@@ -28,23 +28,23 @@ exports.cache = {
  * @type {Object}
  */
 exports.model = {
-  type: 'mysql',
-  common: {
-    logConnect: isDev,
-    logSql: isDev,
-    logger: msg => think.logger.info(msg)
-  },
-  mysql: {
-    handle: mysql,
-    database: 'koa',
-    prefix: '',
-    encoding: 'utf8',
-    host: '127.0.0.1',
-    port: '3306',
-    user: 'root',
-    password: '123456',
-    dateStrings: true,
-  },
+    type: 'mysql',
+    common: {
+        logConnect: isDev,
+        logSql: isDev,
+        logger: msg => think.logger.info(msg)
+    },
+    mysql: {
+        handle: mysql,
+        database: 'koa',
+        prefix: '',
+        encoding: 'utf8',
+        host: 'www.hijs.cc',
+        port: '3306',
+        user: 'koa',
+        password: 'qwertyuiop',
+        dateStrings: true,
+    },
 };
 
 // mongo  ˝æ›ø‚≈‰÷√
@@ -69,18 +69,18 @@ exports.model = {
  * @type {Object}
  */
 exports.session = {
-  type: 'file',
-  common: {
-    cookie: {
-      name: 'thinkjs'
-      // keys: ['werwer', 'werwer'],
-      // signed: true
+    type: 'file',
+    common: {
+        cookie: {
+            name: 'thinkjs'
+            // keys: ['werwer', 'werwer'],
+            // signed: true
+        }
+    },
+    file: {
+        handle: fileSession,
+        sessionPath: path.join(think.ROOT_PATH, 'runtime/session')
     }
-  },
-  file: {
-    handle: fileSession,
-    sessionPath: path.join(think.ROOT_PATH, 'runtime/session')
-  }
 };
 
 /**
@@ -88,15 +88,15 @@ exports.session = {
  * @type {Object}
  */
 exports.view = {
-  type: 'nunjucks',
-  common: {
-    viewPath: path.join(think.ROOT_PATH, 'view'),
-    sep: '_',
-    extname: '.html'
-  },
-  nunjucks: {
-    handle: nunjucks
-  }
+    type: 'nunjucks',
+    common: {
+        viewPath: path.join(think.ROOT_PATH, 'view'),
+        sep: '_',
+        extname: '.html'
+    },
+    nunjucks: {
+        handle: nunjucks
+    }
 };
 
 /**
@@ -104,23 +104,23 @@ exports.view = {
  * @type {Object}
  */
 exports.logger = {
-  type: isDev ? 'console' : 'dateFile',
-  console: {
-    handle: Console
-  },
-  file: {
-    handle: File,
-    backups: 10, // max chunk number
-    absolute: true,
-    maxLogSize: 50 * 1024, // 50M
-    filename: path.join(think.ROOT_PATH, 'logs/app.log')
-  },
-  dateFile: {
-    handle: DateFile,
-    level: 'ALL',
-    absolute: true,
-    pattern: '-yyyy-MM-dd',
-    alwaysIncludePattern: true,
-    filename: path.join(think.ROOT_PATH, 'logs/app.log')
-  }
+    type: isDev ? 'console' : 'dateFile',
+    console: {
+        handle: Console
+    },
+    file: {
+        handle: File,
+        backups: 10, // max chunk number
+        absolute: true,
+        maxLogSize: 50 * 1024, // 50M
+        filename: path.join(think.ROOT_PATH, 'logs/app.log')
+    },
+    dateFile: {
+        handle: DateFile,
+        level: 'ALL',
+        absolute: true,
+        pattern: '-yyyy-MM-dd',
+        alwaysIncludePattern: true,
+        filename: path.join(think.ROOT_PATH, 'logs/app.log')
+    }
 };
