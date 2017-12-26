370be69c334f7b7ba98ee900f9e045e6e71b65a6
wnn <wnn@qq.com>
2017-12-26 18:11:23 +0800

    up

--- src/controller/upload.js
+++ src/controller/upload.js
@@ -9,11 +9,18 @@ const path = require('path');
 const qiniu = require('qiniu');
 
 module.exports = class extends Base {
-  // 打开图片上传页面
-  async indexAction() {
+    // 打开图片上传页面
+    async indexAction() {
 
-    if(this.method === 'POST'){
+        this.assign({
+            'title': '图片上传到本地',
+            'msg': '上传图片'
+        });
+        return this.display();
+    }
 
+    // 图片上传到本地
+    async bduploadAction() {
         let file = this.ctx.file('image'); // 获取file信息
 
         let reader = fs.createReadStream(file.path); // 要被拷贝的源文件
@@ -24,51 +31,43 @@ module.exports = class extends Base {
 
         file.path = __dirname + '/../../www/static/upload' + file.name;
 
-        this.assign('fileInfo', '/static/upload' + file.name);
+        let data = {}
+        data.url = '/static/upload' + file.name
 
-        this.assign({
-            'msg':'上传图片成功'
-        });
+        this.body = {
+            code: 0,
+            data: data,
+            msg: '上传图片成功'
+        };
+    }
 
-        return this.display();
-    }else {
+    // 七牛上传-页
+    async qnuploadAction() {
 
-        this.assign({
-            'title': '图片上传到本地',
-            'msg':'上传图片'
-        });
+        this.assign('title', '七牛图片上传');
         return this.display();
     }
 
-  }
+    // 七牛上传-入库
+    async localToQiniuAction() {
 
-  // 七牛上传-页
-  async qnuploadAction() {
+        let file = this.ctx.file('file');
 
-    this.assign('title', '七牛图片上传');
-    return this.display();
-  }
+        let data = await toolUpload(file.name, file.path);  // 文件上传
 
-  // 七牛上传-入库
-  async localToQiniuAction() {
+        if (data.key) {
 
-    let file = this.ctx.file('file');
+            data.url = 'http://' + this.config('qiniu').domain + '/' + data.key // 回传
 
-    let data = await toolUpload(file.name, file.path);  // 文件上传
-
-    if (data.key) {
-
-      data.url = 'http://' + this.config('qiniu').domain + '/' + data.key // 回传
-
-      this.body = {
-        code: 0,
-        data: data
-      };
-    } else {
-      this.body = {
-        code: -1,
-        data: data
-      };
+            this.body = {
+                code: 0,
+                data: data
+            };
+        } else {
+            this.body = {
+                code: -1,
+                data: data
+            };
+        }
     }
-  }
 };
